14e8a9ddbbcb32f66a6264cc2e9c447eab56fe53
DareenMohamed <dareen922000@gmail.com>
2021-08-28 01:00:03 +0200

    Registartion Page Created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-first-project';
+  username = "";
+  email = "";
+  password = "";
+  users=[{ Username : "Example" , Email : "example@gmail.com" , Password : "m123"}]
+  created=false;
+  constructor(){
+  }
+  addUser(){
+    this.users.push({Username : this.username , Email : this.email , Password : this.password});
+    this.created=true;
+  }
+
 }
